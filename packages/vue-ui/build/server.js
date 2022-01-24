c3b65bded95fb3c29a58a28a7bab8bdc91ca78cc
崔浩 <1791083815@qq.com>
2022-01-24 21:39:14 +0800

    工具

--- packages/vue-ui/build/server.js
+++ packages/vue-ui/build/server.js
@@ -1,32 +1,24 @@
-const fastify = require("fastify")();
+let open = require('open');
+const chalk = require('chalk');
 const webpack = require("webpack");
 const devMiddleware = require("webpack-dev-middleware");
+const fastify = require("fastify")();
 const webpackConfig = require("./webpack.dev");
-const { createFsFromVolume, Volume } = require('memfs');
-
-
-const fs = createFsFromVolume(new Volume());
 
 
 const compiler = webpack(webpackConfig);
-compiler.outputFileSystem = fs;
-console.log(compiler);
+const { publicPath } = webpackConfig.output;
 
-compiler.run((err, stats) => { // [Stats Object](#stats-object)
-    // ...
-    // console.log(stats.entrypoints)
-    const content = fs.readFileSync(compiler.outputPath+'/index.html');
-    console.log(content.toString());
-    compiler.close((closeErr) => {
-        // ...
-    });
+
+const run = async (callback) => {
+    await fastify.register(require("fastify-express"));
+    await fastify.use(devMiddleware(compiler, { publicPath }));
+    await fastify.listen(3001);
+    callback();
+};
+
+
+run(() => {
+    console.log(`服务运行在: ${chalk.blue('http://localhost:3001')}`);
+    open('http://localhost:3001');
 });
-// const { publicPath } = webpackConfig.output;
-
-// const run = async () => {
-//     await fastify.register(require("fastify-express"));
-//     await fastify.use(devMiddleware(compiler, { publicPath }));
-//     await fastify.listen(3001);
-// };
-
-// run();
